b576835fa30176e296ab64ebf5ac8200c5e728d9
riyanada <riyanmaulana402@yahoo.co.id>
2024-02-03 03:05:35 +0700

    init

---
+++ function.py
@@ -0,0 +1,73 @@
+from fastapi import FastAPI, UploadFile, File
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import get_file
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import img_to_array
+from tensorflow import expand_dims
+from tensorflow.nn import softmax
+import tensorflow as tf
+from numpy import argmax
+from numpy import max
+from numpy import array
+from json import dumps
+from uvicorn import run
+import numpy as np
+from PIL import Image
+import os
+import io
+
+class_names = [
+    "sate",
+    "kebab",
+    "air",
+    "jeruk",
+    "ikan",
+    "kentang",
+    "burger",
+    "bubur",
+    "gado",
+    "cumi",
+    "gudeg",
+    "bakwan",
+    "bakso",
+    "rendang",
+    "kopi",
+    "cakwe",
+    "kacang",
+    "apel",
+    "donat",
+    "crepes",
+    "kerupuk",
+    "ayam",
+    "es krim",
+    "durian",
+    "batagor",
+    "anggur",
+    "fu yung hai",
+    "capcay",
+] # 28 class
+
+model_dir = "models/model-v3-Bloss-1706854820.449587.h5"
+model = load_model(model_dir)
+
+
+def preprocess_image(image):
+    image = image.resize((224, 224))  # Resize to match the input size of the model
+    image_array = np.array(image)
+    image_array = tf.keras.applications.xception.preprocess_input(image_array)
+    image_array = np.expand_dims(image, axis=0)
+    return image_array
+
+
+def predict(image):
+    try:
+        # Make the prediction
+        prediction = model.predict(image)
+        predicted_class = np.argmax(prediction)
+
+        class_name = class_names[predicted_class]
+        probability = np.max(prediction) * 100
+    except Exception as e:
+        return {"error": str(e)}
+
+    return {"class": str(class_name), "probability": probability}
---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import io
+import function as func
+from PIL import Image
+from starlette.responses import FileResponse
+from fastapi import FastAPI, UploadFile, File
+from uvicorn import run
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    # "http://172.16.11.113",
+    "http://localhost:8080",
+    # "http://172.16.11.113:8080",
+    # "http://172.16.11.113:8081"/,
+    # Add more allowed origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Defining path operation for root endpoint
+@app.get("/")
+def main():
+    return {"message": "Welcome to Nutriku Model test API!"}
+
+
+# Defining path operation for /documentation endpoint
+@app.get("/documentation")
+async def read_index():
+    return FileResponse("doc/index.html")
+
+
+# Defining path operation for /predict endpoint
+@app.post("/predict")
+async def predict_image(file: UploadFile = File(...)):
+    # Read and preprocess the image
+    content = await file.read()
+    try:
+        image = Image.open(io.BytesIO(content))
+        image = func.preprocess_image(image)
+    except Exception as e:
+        return {"error": str(e)}
+
+    response = func.predict(image)
+    # Return the predicted class
+    return response
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8082))
+    run(app, host="127.0.0.1", port=port, timeout_keep_alive=1200)
